56d858fb2632d8f22a6e406e6f0281e8ed0bb9d2
LaisElessar <laisreisdejesus45@gmail.com>
2024-06-01 19:09:30 -0400

    Fixes content boby space and add style to curriculum button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var mobileMenuIcon = document.querySelector(".botao");
+var mobileMenuIcon = document.querySelector(".button");
 var mobileMenu = document.querySelector('.site_nav_toggle');
 
 mobileMenuIcon.onclick = function(){  
-  if(!mobileMenu.classList.contains('ativo')){
-    mobileMenu.classList.add('ativo')
+  if(!mobileMenu.classList.contains('active')){
+    mobileMenu.classList.add('active')
   
   }else{
-    mobileMenu.classList.remove('ativo')
+    mobileMenu.classList.remove('active')
   }
 }
