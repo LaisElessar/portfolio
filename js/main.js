c0cdf5fc3bf366e75301d5c4723af2c0c4451232
LaisElessar <laisreisdejesus45@gmail.com>
2024-07-11 16:49:56 -0400

    Adds projects page

--- js/main.js
+++ js/main.js
@@ -10,12 +10,36 @@ mobileMenuIcon.onclick = function(){
     mobileMenu.classList.remove('active')
   }
 }
-
-//scroll effect
-document.querySelectorAll('.ancor').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+//slide slick
+$(document).ready(function(){
+  $('.center-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      arrows: true,
+      dots: false,
+      speed: 300,
+      centerPadding: '20px',
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
   });
 });
