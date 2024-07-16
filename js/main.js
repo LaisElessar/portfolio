b41759a6b48e2abb038f44fce1e1aa574877c778
Lais <Laisreisdejesus45@gmail.com>
2024-07-15 21:29:59 -0400

    Fixes mobile arrow slide

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
         {
           breakpoint: 768,
           settings: {
-            arrows: false,
+            arrows: true,
             centerMode: true,
             centerPadding: '40px',
             slidesToShow: 3
@@ -35,7 +35,7 @@ $(document).ready(function(){
         {
           breakpoint: 480,
           settings: {
-            arrows: false,
+            arrows: true,
             centerMode: true,
             centerPadding: '40px',
             slidesToShow: 1
