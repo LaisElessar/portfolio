9c2b0ceefc2f1c9bdbbb165224c6a89931d583e6
Lais <Laisreisdejesus45@gmail.com>
2025-08-15 11:23:09 -0400

    Adds slide on index pagee

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ mobileMenuIcon.onclick = function(){
     mobileMenu.classList.remove('active')
   }
 }
+$('.slider').slick({
+  speed: 3000,            
+  autoplay: true,
+  autoplaySpeed: 0,       
+  cssEase: 'linear',      
+  slidesToShow: 3,        
+  slidesToScroll: 1,
+  infinite: true,         
+  arrows: false,          
+  dots: false,            
+  variableWidth: true     
+});
